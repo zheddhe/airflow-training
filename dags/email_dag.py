b7a00416275032c4a34ee6552756cf7a2cac2ebd
zheddhe <remy.canal@live.fr>
2025-09-06 13:09:59 +0000

    fin bloc 3 (dags & erreurs)

--- dags/email_dag.py
+++ dags/email_dag.py
@@ -2,6 +2,7 @@ import random
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python import PythonOperator
+from airflow.utils.email import send_email
 import datetime
 
 def successful_task():
@@ -10,6 +11,18 @@ def successful_task():
 
 def failed_task():
     raise Exception('This task did not work!')
+
+
+def notify_retry(context):
+    subject = f"[Custom] Retry alert: {context['task_instance']}"
+    html_content = f"<p>Task {context['task_instance']} failed on try #{context['ti'].try_number}</p>"
+    send_email(to=["airflow-email-dag@example.com"], subject=subject, html_content=html_content)
+
+
+def notify_failure(context):
+    subject = f"[Custom] Failure alert: {context['task_instance']}"
+    html_content = f"<p>Task {context['task_instance']} finally failed after retries</p>"
+    send_email(to=["airflow-email-dag@example.com"], subject=subject, html_content=html_content)
 
 
 with DAG(
@@ -33,9 +46,13 @@ with DAG(
         task_id='mail_task',
         python_callable=failed_task,
         retries=5,
-		retry_delay=datetime.timedelta(seconds=10),
-		email_on_retry=True,
-		email=['airflow-email-dag@example.com']
+        retry_delay=datetime.timedelta(seconds=10),
+        email_on_retry=True,
+        # email_on_failure=True,
+        email=['airflow-email-dag@example.com'],
+        # on peut ajouter des callback mail additionnels spécifiques en plus des mail génériques (declenché presence "email=[],")
+        # on_failure_callback=notify_failure,
+        # on_retry_callback=notify_retry,
 )
     # bonne pratique : teardown doit inclure du code idempotent (si re-exécuté : exemple rm -f, DROP IF EXIST)
     cleanup_task = PythonOperator(
