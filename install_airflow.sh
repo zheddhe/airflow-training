962ad2317b6201203ea09b41f39770b3000b6057
zheddhe <remy.canal@live.fr>
2025-09-06 10:11:48 +0000

    Ajout dag additionnels book 3 (dags avancés) et commande airflow

--- install_airflow.sh
+++ install_airflow.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
-#
+
 # Setup Airflow DataScientest env
-#
 wget https://dst-de.s3.eu-west-3.amazonaws.com/airflow_fr/docker-compose/docker-compose.yaml
-
 mkdir -p ./dags ./logs ./plugins
-
 sudo chmod -R 777 logs/
 sudo chmod -R 777 dags/
 sudo chmod -R 777 plugins/
-
 echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
-
 docker-compose up airflow-init
-
 chmod +x start_airflow.sh
+
+# Collect airflow terminal script (use with ./airflow.sh bash)
+wget https://dst-de.s3.eu-west-3.amazonaws.com/airflow_avance_fr/docker-compose/airflow.sh
+chmod +x airflow.sh
