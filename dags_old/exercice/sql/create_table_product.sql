63eae7559590947843add70d62f046feea81fe6c
zheddhe <remy.canal@live.fr>
2025-09-14 12:32:40 +0000

    nettoyage des fichiers non lié à l'examen (presents et suivis sur branche main déjà)

--- dags_old/exercice/sql/create_table_product.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS product (
-  id UUID PRIMARY KEY,
-  name VARCHAR,
-  categories VARCHAR,
-  price REAL,
-  weight REAL,
-  modification_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
